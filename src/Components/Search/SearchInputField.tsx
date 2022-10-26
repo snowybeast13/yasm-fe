946a32ebd233575e518ba86b5a6762ed0910ef14
Uros Miric <roshula13@gmail.com>
2022-10-26 11:04:08 +0200

    finishing search input field, applying styles

--- src/Components/Search/SearchInputField.tsx
+++ src/Components/Search/SearchInputField.tsx
@@ -2,8 +2,21 @@ import React, { FC } from "react";
 
 const SearchInputField: FC = (): JSX.Element => {
   return (
-    <div className="InputWrap">
-      <input className="InputWrap__input-field" type="text" />
+    <div>
+      <div className="InputWrap">
+        <input
+          className="InputWrap__input-field"
+          type="text"
+          placeholder="Search"
+        />
+        <button className="InputWrap__Button">
+          <span className="InputWrap__Button--icon"></span>
+        </button>
+      </div>
+      <p className="InfoText">
+        Hit ENTER to search for your desired skill, employee, organization and
+        more
+      </p>
     </div>
   );
 };
