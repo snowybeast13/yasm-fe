60dc91cc3dab76e169e6c545b46104ebe59d6f06
Uros Miric <roshula13@gmail.com>
2022-11-01 16:49:13 +0100

    deleting chips, clear input data, returning deleted input chips to select dropdown, when clicked outside select field hide select dropdown

--- src/Components/Search/SearchInputField.tsx
+++ src/Components/Search/SearchInputField.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useEffect, useRef, useState } from "react";
 import useDebounce from "../../hooks/useDebounce";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 import { Employee, Person } from "../../Models/interfaces";
 
 interface PersonProps {
@@ -11,14 +12,19 @@ const SearchInputField: FC<PersonProps> = ({
   const [personType, setPersonType] = useState<Person[]>([]);
   const [search, setSearch] = useState<string | null>(null);
   const [inputValue, setInputValue] = useState<Person[]>([]);
+  const [view, setView] = useState<boolean>(false);
+
+  const ref: any = useRef();
+  const inputRef: any = useRef();
 
   const debouncedSearch = useDebounce(search, 500);
+  useOnClickOutside(ref, () => setView(false));
 
   useEffect(() => {
     const typePerson = persons.map((person) => person.person);
 
-    const filter = typePerson.filter((persone) =>
-      persone.name.toLowerCase().includes(debouncedSearch)
+    const filter = typePerson.filter((person) =>
+      person.name.toLowerCase().includes(debouncedSearch)
     );
     if (debouncedSearch) {
       setPersonType(filter);
@@ -28,79 +34,115 @@ const SearchInputField: FC<PersonProps> = ({
     }
   }, [debouncedSearch, persons]);
 
-  // CHIPS INSIDE INPUT FIELD
-  const setUserInput = (f: Person) => {
+  // Chip inside input field
+  const setUserInput = (fPerson: Person) => {
     const newData = [];
-    newData.push(...inputValue, f);
+    newData.push(...inputValue, fPerson);
     setInputValue(newData);
+
+    //When clicked on chip, remove it from the list
+    setPersonType(
+      personType.filter((p) => {
+        return p.id !== fPerson.id;
+      })
+    );
+
+    //Clear input field after chip is added
+    inputRef.current.value = "";
   };
 
-  const showFilteredPersons = () => {
-    console.log("Filtered Persons", inputValue);
+  //Remove chip
+  const removeElement = (index: Person) => {
+    // console.log(index);
+    setInputValue(
+      inputValue.filter((sPerson) => {
+        return sPerson.id !== index.id;
+      })
+    );
+
+    //when clicked on chip return it to the list
+    personType.push(index);
+  };
+
+  //Clear data from input
+  const clearFromInput = (i: Person[]) => {
+    setInputValue([]);
+    const returnPersonToArr = i.filter((rPerson) => {
+      return rPerson;
+    });
+    personType.push(...returnPersonToArr);
   };
 
   return (
     <div>
       <div className="input-wrap">
         <div className="input-wrap__input-div">
-          {inputValue ? (
+          {inputValue && (
             <div className="input-wrap__input-div__chip-wrapper">
               {inputValue.map((singleValue) => (
                 <div
                   key={singleValue.id}
                   className="input-wrap__input-div--chip"
                 >
-                  {singleValue.name}
+                  <p>{singleValue.name}</p>
                   <button
                     onClick={() => {
-                      // deletePerson(singleValue);
+                      removeElement(singleValue);
                     }}
-                  >
-                    x
-                  </button>
+                  ></button>
                 </div>
               ))}
             </div>
-          ) : (
-            <span></span>
           )}
           <input
             className="input-wrap__input-div__input-field"
             type="text"
+            ref={inputRef}
             placeholder="Search"
             onChange={(e) => setSearch(e.target.value)}
+            onClick={() => setView(true)}
           />
         </div>
-        <button className="input-wrap__button" onClick={showFilteredPersons}>
+        <button className="input-wrap__button">
           <span className="input-wrap__button--icon"></span>
         </button>
-      </div>
-      <div>
-        {personType.length ? (
-          <div className="select-wrapper">
-            {personType.map((filteredPersons) => (
-              <div
-                key={filteredPersons.id}
-                className="select-wrapper__items"
-                onClick={() => setUserInput(filteredPersons)}
-              >
-                {filteredPersons.name}
-              </div>
-            ))}
+        {inputValue.length !== 0 && (
+          <div>
+            <button
+              className="input-wrap__clear-button"
+              onClick={() => clearFromInput(inputValue)}
+            ></button>
           </div>
-        ) : (
-          <span></span>
         )}
       </div>
-      <div>
-        {inputValue.map((input) => (
-          <p>{input.name}</p>
-        ))}
-      </div>
+
+      {view && (
+        <div ref={ref} className="select">
+          {personType.length ? (
+            <div className="select__select-wrapper">
+              {personType.map((filteredPersons) => (
+                <div
+                  key={filteredPersons.id}
+                  className="select__select-wrapper__items"
+                  onClick={() => setUserInput(filteredPersons)}
+                >
+                  <p>{filteredPersons.name}</p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="select__select-wrapper">
+              <h3>No options</h3>
+            </div>
+          )}
+        </div>
+      )}
       <p className="info-text">
         Hit ENTER to search for your desired skill, employee, organization and
         more
       </p>
+      {/* Person component goes here */}
+      <div></div>
     </div>
   );
 };
